0896d576d3a53f514e643efd7d2a6ca13106f7d1
masknetdesign <99565662+masknetdesign@users.noreply.github.com>
2025-05-28 00:16:27 -0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 const config = {
-    API_KEY: 'AIzaSyBIUAoStzUUuJL2ZJ1D1xB1JvtCkDXlukY', // Substitua pela sua chave API do Google
+    API_KEY: '', // Substitua pela sua chave API do Google
     API_URL: 'https://generativelanguage.googleapis.com/v1beta/models/imagen-3.0-generate-002:predict'
 };
 
